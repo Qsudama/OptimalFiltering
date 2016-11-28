abfd011cce6ad0b87e760268fbb7afcfc123404b
fedorovpavel <fedorovpavel1992@gmail.com>
2016-11-28 13:04:35 +0300

    fix PinvSVD

--- src/math/linear_algebra.cc
+++ src/math/linear_algebra.cc
@@ -46,26 +46,20 @@ Matrix Cholesky(const Matrix &A, Matrix &L, Matrix &U)
 
 Matrix PinvSVD(const Matrix &A)
 {
-    // WARNING работает с матрицами (n x n) и (m x n) толькое если m > n
-    //         если n > m - падает !
-    Eigen::JacobiSVD<Matrix> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
-
-    Vector singularValues    = svd.singularValues();
-    Vector singularValuesInv = singularValues;
-
-    double max = singularValues(0); //
-    long   m   = (A.rows() > A.cols() ? A.rows() : A.cols());
-    double eps = Const::EPS * m * max;
-
-    for (long i = 0; i < singularValues.size(); ++i) {
-        if (singularValues(i) > eps) {
-            singularValuesInv(i) = 1.0 / singularValues(i);
-        } else {
-            singularValuesInv(i) = 0.0;
-        }
+    Eigen::JacobiSVD<Matrix> svd;
+    if (A.rows() < A.cols()) {
+        svd = Matrix(A.transpose()).jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
+    } else {
+        svd = A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
     }
 
-    return svd.matrixV() * singularValuesInv.asDiagonal() * svd.matrixU().transpose();
+    double max = svd.singularValues().array().abs().maxCoeff();
+    double eps = Const::EPS * std::max(A.cols(), A.rows()) * max;
+
+    Matrix singularValuesInverse =
+        (svd.singularValues().array().abs() > eps).select(svd.singularValues().array().inverse(), 0);
+
+    return svd.matrixV() * singularValuesInverse.asDiagonal() * svd.matrixU().adjoint();
 }
 
 Matrix PinvGreville(const Matrix &A)
