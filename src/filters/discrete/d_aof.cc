d3bd6fa3c3e302eebe8f0b79008912f4e43d8a43
fedorovpavel <fedorovpavel1992@gmail.com>
2016-12-01 12:56:36 +0000

    DiscreteFilters::AOF: fix steps

--- src/filters/discrete/d_aof.cc
+++ src/filters/discrete/d_aof.cc
@@ -39,46 +39,34 @@ void AOF::algorithm()
     Vector h, lambda;
     Matrix G, F, H, Psi;
 
-    // Индекс n соответствует моменту времени tn = t0 + n * dt  (dt - шаг интегрирования):
-    for (size_t n = 1; n < m_result.size(); ++n) {
-        //интегрируем систему (получаем mx, Dx с шагам dt):
-        m_task->setTime(m_result[n - 1].time);
-        m_task->setStep(m_params->integrationStep());
+    m_task->setStep(m_params->measurementStep());
+
+    // Индекс k соответствует моменту времени tk = t0 + k * delta_t  (delta_t - интервал между измерениями):
+    for (size_t k = 1; k < m_result.size(); ++k) {
+        m_task->setTime(m_result[k-1].time);
 
         // Индекс s пробегает по всем элементам выборки:
         for (size_t s = 0; s < m_params->sampleSize(); ++s) {
+            // X_k = a(X_{k-1}); время t = t_{k-1}
             m_sampleX[s] = m_task->a(m_sampleX[s]);
+            // вычисляем lambda, Psi;  время t = t_{k-1}:
+            lambda = m_task->tau(m_sampleZ[s], m_sampleP[s]);
+            Psi    = m_task->Theta(m_sampleZ[s], m_sampleP[s]);
+
+            //ставим текущее (t = tk) время и продолжаем:
+            m_task->setTime(m_result[k].time);
+
+            h = m_task->h(lambda, Psi);
+            G = m_task->G(lambda, Psi);
+            F = m_task->F(lambda, Psi);
+            H = Psi * G.transpose() * Pinv(F);
+
+            m_sampleY[s] = m_task->b(m_sampleX[s]);
+            m_sampleZ[s] = lambda + H * (m_sampleY[s] - h);
+            m_sampleP[s] = Psi - H * G * Psi;
+            m_sampleP[s] = 0.5 * (m_sampleP[s] + m_sampleP[s].transpose());
         }
-        writeResult(n, true);
-
-        // n = 1..K*L*N, если n нацело делится на L*N, значит сейчас время измерения tn = tk:
-        if (n % (m_params->predictionCount() * m_params->integrationCount()) == 0) {
-            double currentTime  = m_result[n].time;
-            double previousTime = currentTime - m_params->measurementStep();
-            m_task->setStep(m_params->measurementStep());
-
-            // Индекс s пробегает по всем элементам выборки:
-            for (size_t s = 0; s < m_params->sampleSize(); ++s) {
-                //вычисляем lambda, Psi: время устонавливаем в предыдущий момент измерения:
-                m_task->setTime(previousTime);
-                lambda = m_task->tau(m_sampleZ[s], m_sampleP[s]);
-                Psi    = m_task->Theta(m_sampleZ[s], m_sampleP[s]);
-
-                //ставим время обратно и продолжаем:
-                m_task->setTime(currentTime);
-
-                h = m_task->h(lambda, Psi);
-                G = m_task->G(lambda, Psi);
-                F = m_task->F(lambda, Psi);
-                H = Psi * G.transpose() * Pinv(F);
-
-                m_sampleY[s] = m_task->b(m_sampleX[s]);
-                m_sampleZ[s] = lambda + H * (m_sampleY[s] - h);
-                m_sampleP[s] = Psi - H * G * Psi;
-                m_sampleP[s] = 0.5 * (m_sampleP[s] + m_sampleP[s].transpose());
-            }
-            writeResult(n);
-        }
+        writeResult(k);
     }
 }
 
