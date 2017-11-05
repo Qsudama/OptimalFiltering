db63d6d5d8cbfdb610d88be991d20a1393015c4b
tercteberc <terctebercDev@yandex.ru>
2017-11-05 23:57:28 +0300

    Fixed start params for task

--- src/tasks/logic_dynamic/ld_landing_rejection_linear.cc
+++ src/tasks/logic_dynamic/ld_landing_rejection_linear.cc
@@ -48,9 +48,14 @@ LandingRejectionLinear::LandingRejectionLinear() : LogicDynamicTask()
     m_varX0(0, 0) = pow(0.015, 2); // Sigma_V_0
     m_varX0(1, 1) = pow(radian, 2); // Sigma_theta_0
     m_varX0(2, 2) = pow(7, 2); // Sigma_H_0
-    m_varX0(3, 3) = pow(CC, 2); // Sigma_c
-    m_varX0(4, 4) = pow(0.02, 2); // Sigma_b
-    m_varX0(5, 5) = pow(radian, 2); // Sigma_varphi
+
+    //    m_varX0(3, 3) = pow(CC, 2); // Sigma_c
+    //    m_varX0(4, 4) = pow(0.02, 2); // Sigma_b
+    //    m_varX0(5, 5) = pow(radian, 2); // Sigma_varphi
+
+    m_varX0(3, 3) = pow(0.0, 2); // Sigma_c
+    m_varX0(4, 4) = pow(0.0, 2); // Sigma_b
+    m_varX0(5, 5) = pow(0.0, 2); // Sigma_varphi
 
     m_varV = Matrix::Zero(m_dimV, m_dimV);
     m_varW = Matrix::Zero(m_dimW, m_dimW);
