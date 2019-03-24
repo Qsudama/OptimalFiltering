db8b5fb4c32ed9f7896451134ac699e5efa63743
Anton Lomakin <terctebercDev@yandex.ru>
2019-03-24 17:24:15 +0300

    Мелкие правки

--- src/core/continuous_filter.cc
+++ src/core/continuous_filter.cc
@@ -43,7 +43,7 @@ void ContinuousFilter::zeroIteration()
         m_sampleZ[s] = mx0;
         m_sampleE[s] = m_sampleX[s] - m_sampleZ[s];
         if(s == trajectoryNumber) {
-            m_realizationE[0] = m_sampleX[s] -  m_sampleZ[s];
+            m_realizationE[0] = m_sampleX[s] - m_sampleZ[s];
         }
     }
 
